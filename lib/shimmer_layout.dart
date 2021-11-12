d67ccc3a91bc682f51a36267f90a6b65f2999b9d
Yaman Aswal <yaman.aswal@ripenapps.in>
2021-11-12 15:20:43 +0530

    initial commit

---
+++ lib/shimmer_layout.dart
@@ -0,0 +1,16 @@
+library shimmer_layout;
+
+import 'package:flutter/material.dart';
+
+/// Shimmer Layout
+class ShimmerLayoutMain extends StatelessWidget {
+  const ShimmerLayoutMain({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("HEllO World");
+  }
+}
+
+
+
---
+++ test/shimmer_layout_test.dart
@@ -0,0 +1,10 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:shimmer_layout/views/components/shimmer_items.dart';
+
+void main(){
+  expect(actual, matcher);
+  runApp(const MaterialApp(home: CircleListItem()));
+}
